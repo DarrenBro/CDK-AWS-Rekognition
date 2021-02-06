d6f4da4846546628a0dd2d074d7517352e625db4
n0237580 <darren.broderick@liberty-it.co.uk>
2021-02-06 21:20:30 +0000

    First commit

--- bin/devhr-project.ts
+++ bin/devhr-project.ts
@@ -3,5 +3,6 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { DevhrProjectStack } from '../lib/devhr-project-stack';
 
+// app construct, initialising app here
 const app = new cdk.App();
 new DevhrProjectStack(app, 'DevhrProjectStack');
--- lib/devhr-project-stack.ts
+++ lib/devhr-project-stack.ts
@@ -1,9 +1,29 @@
 import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
+import dynamodb = require('@aws-cdk/aws-dynamodb');
+import lambda = require('@aws-cdk/aws-lambda');
+import {Duration} from '@aws-cdk/core';
+import iam = require('@aws-cdk/aws-iam');
+import event_sources = require('@aws-cdk/aws-lambda-event-sources');
+import {partition} from "aws-cdk/lib/util";
+
+const imageBucketName = "cdk-rekn-imagebucket"
 
 export class DevhrProjectStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // s3.Bucket is the constructor, passing in 'this' as the scope
+    const imageBucket = new s3.Bucket(this, imageBucketName)
+    new cdk.CfnOutput(this, 'imageBucket', {value: imageBucket.bucketName});
+
+    // dynamoDB - stores the image labels
+    const table = new dynamodb.Table(this, 'ImageLabels', {
+      partitionKey: {name: 'image', type: dynamodb.AttributeType.STRING}
+    });
+    new cdk.CfnOutput(this, 'ddbTable', {value: table.tableName});
+
     // The code that defines your stack goes here
+
   }
 }
